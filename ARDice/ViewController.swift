0368ffa75a23de0c2ad7e6d19fa5ae3edb64350d
Anton Veldanov <anton.veldanov@gmail.com>
2020-04-19 19:57:43 -0700

    iOS

--- ARDice/ViewController.swift
+++ ARDice/ViewController.swift
@@ -20,15 +20,35 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     // Set the view's delegate
     sceneView.delegate = self
     // units in meters
-    let cube = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0.01)
-    let material = SCNMaterial()
-    material.diffuse.contents = UIColor.systemPink
-    cube.materials  = [material]
-    let node = SCNNode()
-    node.position = SCNVector3(0, 0.1, -0.5)
-    node.geometry = cube
+ 
     
-    sceneView.scene.rootNode.addChildNode(node)
+    //MARK: - Sphere - MOON
+    let sphere = SCNSphere(radius: 0.2)
+    
+    let materialSphere = SCNMaterial()
+    materialSphere.diffuse.contents = UIImage(named: "art.scnassets/8k_moon.jpg")
+    sphere.materials = [materialSphere]
+    
+    let nodeSphere = SCNNode()
+    nodeSphere.position = SCNVector3(0.3,0.3,-0.5)
+    nodeSphere.geometry = sphere
+    
+    sceneView.scene.rootNode.addChildNode(nodeSphere)
+
+    
+    
+//    MARK: - CUBE
+//    let cube = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0.01)
+//    let material = SCNMaterial()
+//    material.diffuse.contents = UIColor.systemPink
+//    cube.materials  = [material]
+//    let node = SCNNode()
+//    node.position = SCNVector3(0, 0.1, -0.5)
+//    node.geometry = cube
+//
+//    sceneView.scene.rootNode.addChildNode(node)
+    
+    sceneView.autoenablesDefaultLighting =  true
     
     //        // Create a new scene
     //        let scene = SCNScene(named: "art.scnassets/ship.scn")!
